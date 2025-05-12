ebab4d48e4d0d7917ce29f2b15f5259953df18b7
Amaan Williams <129081794+ST10396397@users.noreply.github.com>
2025-05-12 15:28:24 +0200

    Private lateinit var added to CategoriesActivity and AddExpensesActivity

--- app/src/main/java/com/example/budgetmadnessv20/AddExpensesActivity.kt
+++ app/src/main/java/com/example/budgetmadnessv20/AddExpensesActivity.kt
@@ -1,20 +1,60 @@
 package com.example.budgetmadnessv20
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.*
+import  java.util.*
+import android.app.DatePickerDialog
+import android.content.Intent
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.app.AlertDialog
+import android.graphics.Bitmap
+import android.net.Uri
+import android.provider.MediaStore
+import androidx.core.content.FileProvider
+import java.io.File
 
 class AddExpensesActivity : AppCompatActivity() {
+
+    private lateinit var expenseNameInput: EditText
+    private lateinit var expenseAmountInput: EditText
+    private lateinit var paymentMethodInput: EditText
+    private lateinit var categorySpinner: Spinner
+    private lateinit var selectDataButton: Button
+    private lateinit var uploadReceiptButton: Button
+    private lateinit var addExpenseButton: Button
+    private val PICK_IMAGE_REQUEST = 1
+    private val REQUEST_IMAGE_CAPTURE = 2
+    private var selectedImageBytes: ByteArray? = null
+    private var imageUri: Uri? = null
+
+    private var selectDate: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_add_expenses)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val dbHelper = BudgetDatabaseHelper(this)
+
+        //LINK VIEWS
+
+        //SPINNER SETUP
+        //function logic adapted from:[https://www.geeksforgeeks.org/spinner-in-kotlin/]
+
+
+        //DATE PICKER
+        //Function logic adapted from:[https://www.geeksforgeeks.org/datepicker-in-kotlin/]
+
+
+        //ADD EXPENSE
+        //Function logic adapted from:[https://www.geeksforgeeks.org/android-sqlite-database-in-kotlin/]
+
+
+        //BOTTOM NAVIGATION
+
         }
-    }
+
 }
